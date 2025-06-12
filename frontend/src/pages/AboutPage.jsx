959b97544bc6bdb97026243ca74990f29170d97a
baseer4 <sbaseer003@gmail.com>
2025-06-12 17:52:56 +0530

    Add about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
   return (
-      <div className="flex justify-center items-center min-h-[calc(100vh-5rem)]">
-        <div className="card bg-base-100 w-96 shadow-sm border-2">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-              className="rounded-xl" />
-          </figure>
-          <div className="card-body items-center text-center ">
-            <h2 className="card-title">about</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          </div>
-      </div>
-    </div>
-  )
-}
+    <section className="bg-base-200 h-[calc(100vh-6rem)] py-20 px-6">
+      <div className="max-w-4xl mx-auto text-center space-y-8">
+        <h2 className="text-4xl font-bold text-primary">
+          About ExamGuardAI
+        </h2>
 
-export default AboutPage
+        <p className="text-base-content text-lg">
+          ExamGuardAI is a lightweight, AI-powered proctoring solution that runs right in your browser.
+          No extensions, no downloads — just smart monitoring for online exams.
+        </p>
+
+        <div className="grid md:grid-cols-3 gap-8 text-left mt-15">
+          <div>
+            <h3 className="font-semibold text-base-content mb-2">🎯 Real-Time Face Tracking</h3>
+            <p className="text-md text-base-content">
+              Detects head pose, gaze direction, and face presence during the exam — all in the browser.
+            </p>
+          </div>
+
+          <div>
+            <h3 className="font-semibold text-base-content mb-2">👀 Multi-Person Detection</h3>
+            <p className="text-md text-base-content">
+              Flags multiple people in frame or suspicious side glances that might indicate cheating.
+            </p>
+          </div>
+
+          <div>
+            <h3 className="font-semibold text-base-content mb-2">🔒 Privacy-First</h3>
+            <p className="text-md text-base-content">
+              Analyzes locally, sends only metadata to the backend. No video uploads, no heavy tracking.
+            </p>
+          </div>
+        </div>
+
+        <div className="pt-6 mt-32">
+          <a href="/" className="btn btn-primary btn-wide">
+            Get Started
+          </a>
+        </div>
+
+        <p className="text-sm italic text-base-content/70">
+          "Modern exams deserve modern protection — ExamGuardAI is the silent invigilator you can trust."
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutPage;
