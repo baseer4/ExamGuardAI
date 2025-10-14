5e9acb88d869d11827f3153250a7bcb3eb6ff0c3
baseer4 <sbaseer003@gmail.com>
2025-10-14 15:44:21 +0530

    Fix CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"process.env.VITE_FRONTEND_URL",
+    origin:process.env.VITE_FRONTEND_URL,
     credentials:true,
 }))
 app.use("/api/auth", authRoutes)
